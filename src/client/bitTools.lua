4859e9316ae37daaf276ffe97a3b90d3b7b7adf0
rei <reimakesgames.dev@gmail.com>
2023-03-18 18:12:33 +0800

    structure representation

--- src/client/bitTools.lua
+++ src/client/bitTools.lua
@@ -36,6 +36,51 @@ function bitTools.extract4(n)
 	return bit32.extract(n, FIRST_BYTE, BYTE_SIZE), bit32.extract(n, SECOND_BYTE, BYTE_SIZE), bit32.extract(n, THIRD_BYTE, BYTE_SIZE), bit32.extract(n, FOURTH_BYTE, BYTE_SIZE)
 end
 
+-- a function that lets you get sections of a 32bit number and returns them as a tuple
+function bitTools.loadStructure(n, structure)
+	local tuple = {}
+	for i = 1, #structure do
+		local number = bit32.extract(n, structure[i][1], structure[i][2])
+		table.insert(tuple, number)
+	end
+	return table.unpack(tuple)
+end
+
+function bitTools.saveStructure(structure, data)
+	local n = 0
+	for i = 1, #structure do
+		n = bit32.bor(n, bit32.lshift(data[i], structure[i][1]))
+	end
+
+	return n
+end
+
+function bitTools.representStructure(structure)
+	-- this function is used to represent bitwise structures
+	-- example we just want to use the first 4 bits of a 32bit number,
+	-- we can give it a table of {0, 4} to represent the first 4 bits
+	-- or we can give it a table of {28, 4} to represent the last 4 bits
+
+	local bits = ""
+	local labels = ""
+	-- strucutre is a table of tables that contains the start and length of the bit we want to extract
+	-- though this is just a function to represent the structure, it is not a function to extract the bits
+	for i = 1, #structure do
+		local start = structure[i][1]
+		local length = structure[i][2]
+		for j = 1, length do
+			bits = "X" .. bits
+		end
+		if type(start) == "string" then
+			labels = labels .. start
+		end
+		labels = labels .. " "
+		bits = " " .. bits
+	end
+
+	return labels, bits
+end
+
 function bitTools.to8bitBin(n)
 	return get8bitBin(n)
 end
